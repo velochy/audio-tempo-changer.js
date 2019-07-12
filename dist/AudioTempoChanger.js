2f40479c6e32710bf3b07905f8a1bf6036747a02
Sam Foucart <43978545+samfoucart@users.noreply.github.com>
2019-07-11 22:44:33 -0400

    Fixed issue with flush reading array out of bounds (#3)
    * Fixed issue with flush reading array out of bounds
    
    * updated minor version number
    
    * ran build commands

--- dist/AudioTempoChanger.js
+++ dist/AudioTempoChanger.js
@@ -229,7 +229,7 @@ module.exports = VH;
 
 				// Clear the now-made-future tempo changes (if any)
 				var ci = changes.length-1;
-				while(out_time<=changes[ci].out_time && ci>=0) { changes.pop(); ci--; }
+				while(ci > 0 && out_time <= changes[ci].out_time) { changes.pop(); ci--; }
 
 				// Add a tempo change reflecting current state
 				changes.push({ 
